4b8ff9bc0a7d3ee592f259fae95488c4a9cbca70
underscore11code <minecrafter11mrt@gmail.com>
2021-03-19 09:46:18 -0700

    Code.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # CompSci
 A bunch of Java for usage in my Comp Sci class. Expect messy code.
+
+An online business sells “cool devices” for $250 each. The customer can choose one of three possible delivery methods; priority ($15), express ($12) and standard ($5). Shipping is a one-time cost regardless of the number of “cool devices” ordered.
+
+Create a CoolDevices application that prompts the user for the number of “cool devices” they wish to purchase and the method of shipping they prefer. Calculate the total cost of “cool devices” with shipping. Use the currency format from the NumberFormat class.
+
+## Program Run Sketch:
+
+Enter the number of cool devices you wish to purchase: 
+
+**4**
+
+Which shipping method do you prefer, priority(1), express(2), or standard(3)? 
+
+**3**
+
+Your total cost is $1005 
--- src/main/java/io/github/underscore11code/compsci/Main.java
+++ src/main/java/io/github/underscore11code/compsci/Main.java
@@ -1,7 +1,35 @@
 package io.github.underscore11code.compsci;
 
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    // Do shit here
+    final Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter the number of cool devices you wish to purchase: ");
+    final int quantity = scanner.nextInt();
+
+    System.out.println("Which shipping method do you prefer? Priority (1), Express (2), or Standard (3)?");
+    final int shippingNumber = scanner.nextInt();
+    int shippingCost;
+
+    switch (shippingNumber) {
+      case 1:
+        shippingCost = 15;
+        break;
+      case 2:
+        shippingCost = 12;
+        break;
+      case 3:
+        shippingCost = 5;
+        break;
+      default:
+        System.out.println("Invalid shipping type entered.");
+        return;
+    }
+
+    final int total = (quantity * 250) + shippingCost;
+    System.out.println("Your total cost is " + NumberFormat.getCurrencyInstance().format(total));
   }
 }
